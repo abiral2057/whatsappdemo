fff4f06ca0fa3e3dd8d912de8721f66c53489015
abiral2057 <98310348+abiral2057@users.noreply.github.com>
2023-05-06 00:27:36 +0545

    add

--- app.js
+++ app.js
@@ -66,23 +66,34 @@ function initApp(){
             <img src="image/${value.image}">
             <div class="title">${value.name}</div>
             <div class="price">${value.currency}${value.price.toLocaleString()}</div>
-            <button onclick="addToCard(${key})">Add To Card</button>`;
+            <button onclick="addToCard(${key})" >Add To Card</button>`; //addtocart
+        
         list.appendChild(newDiv);
+        
     })
+    
 }
 initApp();
 function addToCard(key){
+   
     if(listCards[key] == null){
         // copy product form list to list card
         listCards[key] = JSON.parse(JSON.stringify(products[key]));
         listCards[key].quantity = 1;
+        
+       
     }
+   
+    
     reloadCard();
 }
 function reloadCard(){
     listCard.innerHTML = '';
     let count = 0;
     let totalPrice = 0;
+
+    alert("Product Added to Card successfully"); //alertshow
+
     listCards.forEach((value, key)=>{
         totalPrice = totalPrice + value.price;
         count = count + value.quantity;
@@ -90,19 +101,20 @@ function reloadCard(){
             let newDiv = document.createElement('li');
             newDiv.innerHTML = `
                 <div><img src="image/${value.image}"/></div>
-                <div>${value.name} |</div>
+                <div>${value.name}-<br></div>
                 <div>${value.currency}${value.price.toLocaleString()}</div>
                 <div>
                     <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
-                    <div class="count">${value.quantity}</div>
+                    <div class="count"> ${value.quantity} ||</div>
                     <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
                 </div>`;
                 listCard.appendChild(newDiv);
+                
         }
     })
-    total.innerText = totalPrice.toLocaleString();
+    total.innerText = "Rs."+totalPrice.toLocaleString()+"/-";
     quantity.innerText = count;
-  
+
 }
 function changeQuantity(key, quantity){
     if(quantity == 0){
